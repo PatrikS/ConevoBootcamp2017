2465b6cb7faf53834677111465813fbdc08737d7
Peter <pewa0500@outlook.com>
2017-06-02 10:09:07 +0200

    The Test!

---
+++ TheTest/src/TheTest.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheTest.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+}
