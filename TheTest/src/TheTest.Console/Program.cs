384034decd51abb633ff08d8a089cef993304a42
PatrikS <patrik.n.svensson@gmail.com>
2017-06-02 10:48:38 +0200

    press the any key

--- TheTest/src/TheTest.Console/Program.cs
+++ TheTest/src/TheTest.Console/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TheTest.Console
+﻿namespace TheTest.Console
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            System.Console.WriteLine("Hello World!");
+
+            System.Console.WriteLine("press the 'any' key");
             System.Console.ReadKey();
         }
     }
